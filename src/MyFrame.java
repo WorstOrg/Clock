7e11ea6aebae13485047916ada2846de78e8f472
WorstProgrammerEver45 <tuhindiscord@gmail.com>
2021-09-27 13:09:45 +0600

    lmao

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -2,6 +2,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
@@ -15,12 +16,11 @@ public class MyFrame extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Clock");
         this.setLayout(new FlowLayout());
-        this.setSize(350, 200);
+        this.setSize(450, 300);
         this.setResizable(false);
         timeFormat = new SimpleDateFormat("hh:mm:ss a");
         timeLabel = new JLabel();
-        time = timeFormat.format(Calendar.getInstance().getTime());
-        timeLabel.setText(time);
+        timeLabel.setFont(new Font("MV Boli", Font.PLAIN, 40));
         this.add(timeLabel);
         this.setVisible(true);
 
@@ -29,5 +29,15 @@ public class MyFrame extends JFrame {
     }
 
     public void setTime() {
+        while (true) {
+            time = timeFormat.format(Calendar.getInstance().getTime());
+            timeLabel.setText(time);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 }
